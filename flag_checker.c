d6866ffc426f164bbf19f3e3539c0046b360b414
Bennyonye <benedictigbukolu@gmail.com>
2023-08-20 14:20:31 -0700

    Create flag_checker.c - By Benedict

---
+++ flag_checker.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_flags - Calculates active flags from the format string.
+ * @format: Formatted string in which to check for flags.
+ * @i: Pointer to the index where flag checking starts.
+ * Return: Bitwise representation of the set flags.
+ */
+int get_flags(const char *format, int *i)
+{
+    /* Mapping of flags to their corresponding bitwise values. */
+    const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+    const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+    int j;
+    int flags = 0;
+
+    for (*i = *i + 1; format[*i] != '\0'; (*i)++)
+    {
+        int found = 0;
+        for (j = 0; FLAGS_CH[j] != '\0'; j++)
+        {
+            if (format[*i] == FLAGS_CH[j])
+            {
+                flags |= FLAGS_ARR[j];
+                found = 1;
+                break;
+            }
+        }
+
+        /* If no flag character is found, break from the loop. */
+        if (!found)
+        {
+            break;
+        }
+    }
+
+    /* Move the pointer back to the last checked character. */
+    (*i)--;
+
+    return flags;
+}
