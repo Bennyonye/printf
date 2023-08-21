e8e17fb78e30139cb0396cfbf64468915562e31d
Benedict Onye <98492400+Bennyonye@users.noreply.github.com>
2023-08-21 11:53:02 +0100

    Updated _printf.c - by Benedict

--- _printf.c
+++ _printf.c
@@ -11,47 +11,46 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: The format string.
  * Return: Number of characters printed.
  */
-int _printf(const char *format, ...)
-{
-    int i, printed_chars = 0, buff_ind = 0;
-    int flags, width, precision, size;
-    va_list list;
-    char buffer[BUFF_SIZE];
 
-    if (format == NULL)
-        return (-1);
-
-    va_start(list, format);
-
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
+        int _printf(const char *format, ...)
         {
-            buffer[buff_ind++] = format[i];
-            if (buff_ind == BUFF_SIZE)
-                print_buffer(buffer, &buff_ind);
-
-            printed_chars++;
-        }
-        else
-        {
-            print_buffer(buffer, &buff_ind);
+            int i, printed_chars = 0, buff_ind = 0, printed;
+            int flags, width, precision, size;
+            va_list list;
+            char buffer[BUFF_SIZE];
             
-            flags = get_flags(format, &i);
-            width = get_width(format, &i, list);
-            precision = get_precision(format, &i, list);
-            size = get_size(format, &i);
-
-            /* Move past the current character. */
-            i++;
-
-            int printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
-            if (printed == -1)
+            if (format == NULL)
                 return (-1);
-
-            printed_chars += printed;
-        }
-    }
+            
+            va_start(list, format);
+            
+            for (i = 0; format[i] != '\0'; i++)
+            {
+                if (format[i] != '%')
+                {
+                    buffer[buff_ind++] = format[i];
+                    if (buff_ind == BUFF_SIZE)
+                        print_buffer(buffer, &buff_ind);
+                    
+                    printed_chars++;
+                }
+                else
+                {
+                    print_buffer(buffer, &buff_ind);
+                    flags = get_flags(format, &i);
+                    width = get_width(format, &i, list);
+                    precision = get_precision(format, &i, list);
+                    size = get_size(format, &i);
+                    
+                    /* Move past the current character. */
+                    i++;
+                    
+                    printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
+                    if (printed == -1)
+                        return (-1);
+                    printed_chars += printed;
+                }
+            }
 
     print_buffer(buffer, &buff_ind);
 
