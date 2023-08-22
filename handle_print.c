4c3918eff3c45319743793afc656b3472db7d52a
Benedict Onye <98492400+Bennyonye@users.noreply.github.com>
2023-08-22 14:30:00 +0100

    Created by Daniel - Update handle_print.c - by Benedict

--- handle_print.c
+++ handle_print.c
@@ -1,26 +1,22 @@
 #include "main.h"
 
-/* ============================ FUNCTION ============================ */
 /**
- * handle_print - Prints an argument based on its type.
+ * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
  * @list: List of arguments to be printed.
- * @ind: Pointer to the current index in the format string.
+ * @ind: Index to format type in string.
  * @buffer: Buffer array to handle print.
- * @flags: Flag specifier.
- * @width: Width specifier.
- * @precision: Precision specifier.
+ * @flags: Active flags.
+ * @width: Width of print.
+ * @precision: Precision specification.
  * @size: Size specifier.
  *
- * Return: Number of characters printed.
+ * Return: Number of printed characters or -1 on error.
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-    /* ----------- VARIABLES DECLARATION ----------- */
-	int i, unknow_len = 0, printed_chars = -1;
-	
-	/* Struct to map format specifier to its corresponding function */
+	int i, unknown_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -28,31 +24,30 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-    
-    /* ----------- PROCESS FORMAT SPECIFIERS ----------- */
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	/* ----------- HANDLE UNKNOWN FORMAT SPECIFIERS ----------- */
-	if (fmt_types[i].fmt == '\0')
-	{
-		if (fmt[*ind] == '\0')
-			return (-1);
-		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
-		else if (width)
-		{
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
-			return (1);
+	for (i = 0; fmt_types[i].fmt != '\0'; i++) {
+		if (fmt[*ind] == fmt_types[i].fmt) {
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 		}
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
 	}
-	return (printed_chars);
+
+	// No format specifier was found
+	if (fmt[*ind] == '\0')
+		return (-1);
+
+	unknown_len += write(1, "%%", 1);
+
+	if (fmt[*ind - 1] == ' ')
+		unknown_len += write(1, " ", 1);
+	else if (width) {
+		--(*ind);
+		while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+			--(*ind);
+		if (fmt[*ind] == ' ')
+			--(*ind);
+		return (1);
+	}
+
+	unknown_len += write(1, &fmt[*ind], 1);
+	return (unknown_len);
 }
